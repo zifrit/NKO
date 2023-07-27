6febb12e72b1a2e976ad28dcdfd24bbe703d6450
zifrit <zifrit@list.ru>
2023-07-27 17:10:42 +0300

    .

---
+++ NKO/celery.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NKO.settings')
+
+app = Celery('nko')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.broker_url = 'redis://redis:6379/0'
+app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    time.sleep(20)
+    print(f'Request: {self.request!r}')
+    print('Hello form debug_task')
