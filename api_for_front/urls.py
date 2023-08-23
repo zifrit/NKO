172e175ab3b2f3af88ecc6e9db64438f49c7cb99
zifrit <zifrit@list.ru>
2023-08-23 19:38:49 +0300

    fixed

--- api_for_front/urls.py
+++ api_for_front/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 from . import views
 
 router = routers.SimpleRouter()
-router.register(r'project', views.CRUDProjectViewSet)
+router.register(r'project', views.MainProjectViewSet)
 router.register(r'steps', views.ListRetrieveStep)
+router.register(r'links', views.LinkStepViewSet)
 
 urlpatterns = [
     # path('liststage/', views.ViewListStage.as_view()),
     path('create_step_template_schema/', views.CreateTemplatesStep.as_view()),
-    path('list_create_main_table/', views.ListCreateMainTableKo.as_view()),
     path('create_step/', views.CreateStep.as_view()),
     path('update_stage_info/<int:pk>/', views.AddInfoInStage.as_view()),
     path('', include(router.urls)),
