3e40d328d059bb3c307276ef7d2e9f9666d06f1f
zifrit <zifrit@list.ru>
2023-08-23 19:51:08 +0300

    customize admin

--- api_for_front/admin.py
+++ api_for_front/admin.py
@@ -9,17 +9,41 @@ from . import models
 class MainKO(admin.ModelAdmin):
     list_display = ['id', 'name', 'user']
     list_display_links = ['id', 'name']
+    save_on_top = True
+    list_per_page = 30
 
 
 @admin.register(models.Step)
 class MainKO(admin.ModelAdmin):
     list_display = ['id', 'name']
     list_display_links = ['id', 'name']
+    search_fields = ['name']
+    save_on_top = True
+    list_per_page = 30
 
 
-admin.site.register(models.FieldText)
-admin.site.register(models.LinksStep)
-admin.site.register(models.StepTemplates)
-admin.site.register(models.FieldTextarea)
-admin.site.register(models.FieldDate)
-admin.site.register(models.FieldStartFinishTime)
+@admin.register(models.LinksStep)
+class MainKO(admin.ModelAdmin):
+    list_display = ['id', 'description', 'start_id', 'end_id', 'color']
+    list_display_links = ['id', 'description']
+    save_on_top = True
+    list_per_page = 30
+
+
+@admin.register(models.StepTemplates)
+class MainKO(admin.ModelAdmin):
+    list_display = ['id', 'name', 'user']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    save_on_top = True
+    list_per_page = 30
+
+
+class BaseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'identify', 'name_field']
+
+
+admin.site.register(models.FieldText, BaseAdmin)
+admin.site.register(models.FieldTextarea, BaseAdmin)
+admin.site.register(models.FieldDate, BaseAdmin)
+admin.site.register(models.FieldStartFinishTime, BaseAdmin)
