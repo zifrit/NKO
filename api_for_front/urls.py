3aa07971c1423489080811277237d8883a5babb3
zifrit <zifrit@list.ru>
2023-09-22 19:02:00 +0300

    .

--- api_for_front/urls.py
+++ api_for_front/urls.py
@@ -5,13 +5,12 @@ from . import views
 
 router = routers.SimpleRouter()
 router.register(r'project', views.MainProjectViewSet)
-router.register(r'steps', views.ListStep)
+router.register(r'steps', views.Steps)
 router.register(r'links', views.LinkStepViewSet)
 
 urlpatterns = [
     path('create-step-template-schema/', views.CreateTemplatesStep.as_view()),
     path('list-schema/', views.ListSchema.as_view()),
-    path('create-step/', views.CreateStep.as_view()),
     path('update-step_info/', views.AddInfoInStage.as_view()),
     path('', include(router.urls)),
 ]
