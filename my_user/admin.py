42c4b593ff8b3b42b48419c9d6907e1cc155c594
zifrit <zifrit@list.ru>
2023-09-28 17:37:28 +0300

    added user profile and register it in admin panel

--- my_user/admin.py
+++ my_user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from . import models
 
-# Register your models here.
+
+class UserProfileInline(admin.TabularInline):
+    model = models.UserProfile
+    extra = 1
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- my_user/models.py
+++ my_user/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=255, verbose_name='фамилия')
+
+    class Meta:
+        db_table = 'UserProfile'
+        verbose_name = 'UserProfile'
+        verbose_name_plural = 'UserProfiles'
+
