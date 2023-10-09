92103b5117ff8e14e417bcad4f3ee779715af717
zifrit <zifrit@list.ru>
2023-10-09 16:59:13 +0300

    fixed endpoints

--- api_for_front/urls.py
+++ api_for_front/urls.py
@@ -12,9 +12,9 @@ router.register(r'templates', views.TemplatesStep)
 urlpatterns = [
     path('step-filed/delete/<int:pk>/', views.DeleteStepFiled.as_view(), name='step_filed_delete'),
     path('departments/', views.GetDepartmentView.as_view(), name='get_departments'),
-    path('departments/', views.CreateDepartmentView.as_view(), name='post_departments'),
+    path('departments/create/', views.CreateDepartmentView.as_view(), name='post_departments'),
     path('departments/<int:pk>/', views.DepartmentUserView.as_view(), name='users_in_departments'),
-    path('departments/<int:pk>/', views.DeleteDepartmentView.as_view(), name='delete_departments'),
+    path('departments/delete/<int:pk>/', views.DeleteDepartmentView.as_view(), name='delete_departments'),
     path('departments/add_or_remove_users/', views.AddToDepartmentUserView.as_view(),
          name='add_or_remove_users_in_departments'),
     path('replacement-step-place/<int:pk>/', views.ReplacementPlaceStep.as_view(), name='replacement_step_place'),
