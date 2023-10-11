8d697b31469e4c6bb392a00b74686c7566b7421d
zifrit <zifrit@list.ru>
2023-10-11 19:19:00 +0300

    added test CRUDTemplates

--- api_for_front/tests.py
+++ api_for_front/tests.py
@@ -12,7 +12,6 @@ from . import models
 class CRUDLinkStep(TestCase):
     @classmethod
     def setUpClass(cls):
-        super(CRUDLinkStep, cls).setUpClass()
         cls.links_1 = models.LinksStep.objects.create(start_id=1, end_id=2, description='test1', color='Black', data={})
         cls.links_2 = models.LinksStep.objects.create(start_id=3, end_id=4, description='test2', color='white', data={})
         cls.links_3 = models.LinksStep.objects.create(start_id=5, end_id=6, description='test3', color='yellow',
@@ -20,7 +19,6 @@ class CRUDLinkStep(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        super(CRUDLinkStep, cls).tearDownClass()
         cls.links_1.delete()
         cls.links_2.delete()
         cls.links_3.delete()
@@ -118,7 +116,6 @@ class CRUDProject(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        super(CRUDProject, cls).tearDownClass()
         cls.pro_1.delete()
         cls.pro_2.delete()
         cls.pro_3.delete()
@@ -189,4 +186,116 @@ class CRUDProject(TestCase):
 
     def test_delete(self):
         response = self.client.delete(reverse('mainproject-detail', kwargs={"pk": self.pro_1.pk}))
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+
+
+class CRUDTemplates(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(CRUDTemplates, cls).setUpClass()
+        cls.user = User.objects.create_user(username='test1', password='test1')
+        cls.templates1 = models.StepTemplates.objects.create(name='template1', user_id=1,
+                                                             schema={"type": "text1",
+                                                                     "data": {
+                                                                         "identify": "type_filed1"
+                                                                     }
+                                                                     }
+                                                             )
+        cls.templates2 = models.StepTemplates.objects.create(name='template2', user_id=1,
+                                                             schema={"type": "text2",
+                                                                     "data": {
+                                                                         "identify": "type_filed2"
+                                                                     }
+                                                                     }
+                                                             )
+        cls.templates3 = models.StepTemplates.objects.create(name='template3', user_id=1,
+                                                             schema={"type": "text3",
+                                                                     "data": {
+                                                                         "identify": "type_filed3"
+                                                                     }
+                                                                     }
+                                                             )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.templates1.delete()
+        cls.templates2.delete()
+        cls.templates3.delete()
+
+    def test_create_templates(self):
+        response = self.client.post(reverse('steptemplates-list'),
+                                    {
+                                        "name": "roomtemplate",
+                                        "schema": json.dumps([
+                                            {
+                                                "type": "text",
+                                                "data": {
+                                                    "identify": "type_filed"
+                                                }
+                                            }
+                                        ]),
+                                        "user": 1
+                                    }
+                                    )
+        expected_data = {
+            "id": 4,
+            "name": "roomtemplate",
+            "schema": [
+                {
+                    "type": "text",
+                    "data": {
+                        "identify": "type_filed"
+                    }
+                }
+            ],
+            "user": 1
+        }
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertJSONEqual(response.content, expected_data)
+
+    def test_get_list_templates(self):
+        response = self.client.get(reverse('steptemplates-list'))
+        expected_data = [
+            {
+                "id": 1,
+                "name": "template1",
+                "schema":
+                    {
+                        "type": "text1",
+                        "data": {
+                            "identify": "type_filed1"
+                        }
+                    },
+                "user": 1
+            },
+            {
+                "id": 2,
+                "name": "template2",
+                "schema":
+                    {
+                        "type": "text2",
+                        "data": {
+                            "identify": "type_filed2"
+                        }
+                    },
+                "user": 1
+            },
+            {
+                "id": 3,
+                "name": "template3",
+                "schema":
+                    {
+                        "type": "text3",
+                        "data": {
+                            "identify": "type_filed3"
+                        }
+                    },
+                "user": 1
+            }
+        ]
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertJSONEqual(response.content, expected_data)
+
+    def test_delete(self):
+        response = self.client.delete(reverse('steptemplates-detail', kwargs={"pk": self.templates1.pk}))
         self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
