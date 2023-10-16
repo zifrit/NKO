c5a340ecbd69be7341c3fa6c31db0a912a71bf6b
zifrit <zifrit@list.ru>
2023-10-16 18:50:27 +0300

    fixed admin

--- api_for_front/admin.py
+++ api_for_front/admin.py
@@ -7,7 +7,8 @@ from . import models
 
 @admin.register(models.MainProject)
 class MainKO(admin.ModelAdmin):
-    list_display = ['id', 'name', 'user']
+    list_display = ['id', 'name', 'user', 'active']
+    list_editable = ['active']
     list_display_links = ['id', 'name']
     search_fields = ['name']
     save_on_top = True
@@ -15,8 +16,8 @@ class MainKO(admin.ModelAdmin):
 
 
 @admin.register(models.Step)
-class MainKO(admin.ModelAdmin):
-    list_display = ['id', 'name', 'project_id_id']
+class StepAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'project_id']
     list_display_links = ['id', 'name']
     search_fields = ['name']
     save_on_top = True
@@ -24,7 +25,7 @@ class MainKO(admin.ModelAdmin):
 
 
 @admin.register(models.StepFields)
-class MainKO(admin.ModelAdmin):
+class StepFieldsAdmin(admin.ModelAdmin):
     list_display = ['id', 'step']
     list_display_links = ['id', 'step']
     search_fields = ['step__name']
@@ -33,7 +34,7 @@ class MainKO(admin.ModelAdmin):
 
 
 @admin.register(models.StepFiles)
-class MainKO(admin.ModelAdmin):
+class StepFilesAdmin(admin.ModelAdmin):
     list_display = ['id', 'link_step', 'link_field']
     list_display_links = ['id', 'link_step', 'link_field']
     search_fields = ['step__name']
@@ -41,7 +42,7 @@ class MainKO(admin.ModelAdmin):
 
 
 @admin.register(models.LinksStep)
-class MainKO(admin.ModelAdmin):
+class LinksStepAdmin(admin.ModelAdmin):
     list_display = ['id', 'description', 'start_id', 'end_id', 'color']
     list_display_links = ['id', 'description']
     save_on_top = True
@@ -49,7 +50,7 @@ class MainKO(admin.ModelAdmin):
 
 
 @admin.register(models.StepTemplates)
-class MainKO(admin.ModelAdmin):
+class StepTemplatesAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'user']
     list_display_links = ['id', 'name']
     search_fields = ['name']
